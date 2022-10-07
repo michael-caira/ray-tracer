8941214076fd688ee3e8cf9e22c4cecf2c301a84
Michael Caira <mbcaira@gmail.com>
2022-10-06 23:41:06 -0400

    initial vec3 header

--- main.cc
+++ main.cc
@@ -13,9 +13,10 @@ main()
   cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
   for (int i = image_height - 1; i >= 0; --i) {
+    cerr << "\rScanlines remaining: " << i << ' ' << flush;
     for (int j = 0; j < image_width; ++j) {
-      auto r = double(i) / (image_width - 1);
-      auto g = double(j) / (image_height - 1);
+      auto r = double(j) / (image_width - 1);
+      auto g = double(i) / (image_height - 1);
       auto b = 0.25;
 
       int ir = static_cast<int>(255.999 * r);
@@ -25,6 +26,6 @@ main()
       cout << ir << ' ' << ig << ' ' << ib << endl;
     }
   }
-
+  cerr << "\nDone.\n";
   return 0;
 }
